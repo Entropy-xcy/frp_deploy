0bfddd8af595e358c11cfd7a0e8e9796a0507ee4
root <root@ip-172-26-13-140.ap-southeast-1.compute.internal>
2019-08-02 05:21:37 +0000

    Add server restart

---
+++ server_restart.sh
@@ -0,0 +1,13 @@
+killall frps
+python3 server_ini_gen.py
+sudo nohup ./frps -c frps0.ini > frps0.log &
+sudo nohup ./frps -c frps1.ini > frps1.log &
+sudo nohup ./frps -c frps2.ini > frps2.log &
+sudo nohup ./frps -c frps3.ini > frps3.log &
+sudo nohup ./frps -c frps4.ini > frps4.log &
+sudo nohup ./frps -c frps5.ini > frps5.log &
+sudo nohup ./frps -c frps6.ini > frps6.log &
+sudo nohup ./frps -c frps7.ini > frps7.log &
+sudo nohup ./frps -c frps8.ini > frps8.log &
+sudo nohup ./frps -c frps9.ini > frps9.log &
+
